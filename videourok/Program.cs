2bf2d490a1ef625d09628ec7b0c1fd0b91426f05
Egor Maleev <egor.maleev@list.ru>
2020-06-18 22:57:21 +0500

    egorka

---
+++ videourok/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace videourok
+{
+    
+        class Person
+        {
+            private string _firstName;
+            private string _lastName;
+
+            public Person()
+            {
+                _firstName = "Tom";
+            }
+
+            public Person(string name, string surname)
+            {
+                _firstName = name;
+                _lastName = surname;
+            }
+
+            public string FirstName
+            {
+                get { return _firstName; }
+                set { _firstName = value; }
+            }
+
+            public string LastName
+            {
+                get { return _lastName; }
+                set { _lastName = value; }
+            }
+
+            public void Display()
+            {
+                Console.WriteLine(FirstName + " " + LastName);
+            }
+        }
+
+        class Employee : Person
+        {
+            private string _company;
+
+            public Employee(string name, string surname, string company) : base(name, surname)
+            {
+                Company = company;
+            }
+
+            public string Company
+            {
+                get { return _company; }
+                set { _company = value; }
+            }
+
+        }
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Person person = new Person("Малевич", "Егорка");
+                Employee employee = new Employee("Малеев", "Квадрат", "МойМир Майлру");
+
+                person.Display();
+                employee.Display();
+
+                Console.ReadKey();
+            }
+        }
+    }
